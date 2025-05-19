346f02d7acbb25ba0f0935062a8f77aaa5cdb20d
姚睿波 <yaoruibo@yaoruibodeMacBook-Pro.local>
2025-05-19 16:55:27 +0800

    📂 Upload folder on 20250519_165527

---
+++ 测试代码3/Services/LogService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace CMTest2.Services
+{
+    [Export(typeof(LogService))]
+    //[Export]
+    public class LogService
+    {
+
+        private readonly Dispatcher _dispatcher;
+        public ObservableCollection<string> Logs { get; private set; }
+
+        public LogService()
+        {
+            Logs = new ObservableCollection<string>();
+            _dispatcher = Application.Current?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+        }
+
+        public void WriteLog(string message)
+        {
+            _dispatcher.Invoke(() =>
+            {
+                Logs.Add($"[{DateTime.Now:HH:mm:ss:fff}] {message}");
+            });
+        }
+    }
+}
---
+++ 测试代码3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Threading;
+using Caliburn.Micro;
+using CMTest2.Services;
+
+namespace CMTest2.ViewModels
+{
+    [Export(typeof(MainWindowViewModel))]
+    public class MainWindowViewModel : Screen
+    {
+        [Import]
+        private LogService _logService;
+        public ObservableCollection<string> Logs => _logService.Logs;
+
+
+        public AutoResetEvent Line1Module1Event = new AutoResetEvent(false);
+        public AutoResetEvent Line2Module1Event = new AutoResetEvent(false);
+        public AutoResetEvent Line1FlowEvent = new AutoResetEvent(true);
+        public AutoResetEvent Line2FlowEvent = new AutoResetEvent(true);
+        private static readonly Random rand = new Random(); 
+        public object Module1 = new object(); 
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public void StartTasks()
+        {
+            var Thread1 = new Thread(Line1Mission);
+            var Thread2 = new Thread(Line2Mission);
+            var Thread3 = new Thread(Line1);
+            var Thread4 = new Thread(Line2);
+
+            Thread1.Start();
+            Thread2.Start();
+            Thread3.Start();
+            Thread4.Start();
+
+            _logService.WriteLog("已启动 4 个线程。");
+        }
+
+        private void Line1Mission()
+        {
+            while (true)
+            {
+                Line1Module1Event.WaitOne();
+                lock (Module1)
+                {
+                    _logService.WriteLog($"Line1撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line1撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line1撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line1撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    Line1FlowEvent.Set();
+                }
+
+            }
+        }
+
+        private void Line2Mission()
+        {
+            while (true)
+            {
+                Line2Module1Event.WaitOne();
+                lock (Module1)
+                {
+                    _logService.WriteLog($"Line2撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line2撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line2撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    _logService.WriteLog($"Line2撕膜任务执行中");
+                    Thread.Sleep(1000);
+                    Line2FlowEvent.Set();
+                }
+
+            }
+        }
+
+        private void Line1()
+        {
+            while (true)
+            {
+                Line1FlowEvent.WaitOne();
+
+                int time = rand.Next(1, 6) * 1000; 
+                Thread.Sleep(time);
+
+                Line1Module1Event.Set();
+            }
+        }
+
+        private void Line2()
+        {
+            while (true)
+            {
+                Line2FlowEvent.WaitOne();
+
+                int time = rand.Next(1, 11) * 1000;
+                Thread.Sleep(time);
+
+                Line2Module1Event.Set();
+            }
+        }
+    }
+}
